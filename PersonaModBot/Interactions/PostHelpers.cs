a624e59d78e5af8e4985d85d774614dc813db743
AnimatedSwine37 <24914353+AnimatedSwine37@users.noreply.github.com>
2023-01-06 15:39:31 +1000

    Add project files.

---
+++ PersonaModBot/Interactions/PostHelpers.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonaModBot.Interactions
+{
+    public class PostHelpers : InteractionModuleBase
+    {
+        [SlashCommand("solved", "Mark the current post as solved")]
+        public async Task Solved()
+        {
+            if (Context.Channel.GetChannelType() != ChannelType.PublicThread)
+            {
+                await RespondAsync("You can only solve posts in forums.", ephemeral: true);
+                return;
+            }
+
+            SocketThreadChannel channel = (SocketThreadChannel)Context.Channel;
+            var parent = channel.ParentChannel;
+            if(parent.GetChannelType() != ChannelType.Forum)
+            {
+                await RespondAsync("You can only solve posts in forums.", ephemeral: true);
+                return;
+            }
+
+            SocketForumChannel forum = (SocketForumChannel)parent;
+
+            var solvedTag = forum.Tags.FirstOrDefault(tag => tag.Name == "Solved");
+
+            var currentTags = channel.AppliedTags;
+
+            if(currentTags.Contains(solvedTag.Id))
+            {
+                await RespondAsync("This post is already tagged as solved.", ephemeral: true);
+                return;
+            }
+
+            await channel.ModifyAsync(x =>
+            {
+                    x.AppliedTags = Optional.Create(currentTags.Append(solvedTag.Id));
+            });
+
+            await RespondAsync("This issue has been solved! (This is hopefully true)");
+        }
+    }
+}
---
+++ PersonaModBot/Program.cs
@@ -0,0 +1,55 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace PersonaModBot
+{
+    public class Program
+    {
+        private readonly IServiceProvider _serviceProvider;
+        
+        public Program()
+        {
+            _serviceProvider = CreateServices();
+        }
+
+        static void Main(string[] args)
+            => new Program().RunAsync(args).GetAwaiter().GetResult();
+
+        static IServiceProvider CreateServices()
+        {
+            var collection = new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<InteractionService>()
+                .AddSingleton<InteractionHandler>();
+
+            return collection.BuildServiceProvider();
+        }
+
+        async Task RunAsync(string[] args)
+        {
+            var client = _serviceProvider.GetRequiredService<DiscordSocketClient>();
+            var interactionHandler = _serviceProvider.GetRequiredService<InteractionHandler>();
+
+            client.Log += async (msg) =>
+            {
+                await Task.CompletedTask;
+                Console.WriteLine(msg);
+            };
+
+            client.Ready += interactionHandler.InitialiseAsync;
+
+
+            // TODO remove before committing!
+            var token = File.ReadAllText("token.txt");
+            
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+    }
+}
